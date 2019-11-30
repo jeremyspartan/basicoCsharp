1b2d9005f84fefbf072bf101dd8060fde903e7a3
jeremyspartan <jyvebuz@gamil.com>
2019-11-30 09:47:57 -0600

    se trabaja en operaciones aritmeticas y de asignacion en c#

--- BasicoCsharp/Program.cs
+++ BasicoCsharp/Program.cs
@@ -6,7 +6,30 @@ namespace BasicoCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int annioActual = Convert.ToInt32("2019");
+            double numPI = 3.141592;
+            Console.WriteLine("Valores {0} -- {1}", annioActual, numPI);
+            decimal montoFact = 94593.34m + 0.07m;
+            float impVenta = 9239.04f;
+            Console.WriteLine("Valores {0} -- {1}",montoFact,impVenta);
+            double resultadoSum = 540 + -18.5;
+            float resultadoResta = 3464.55f + 334.34f;
+            decimal resultadoMult = 23.545m * 4243m;
+            decimal resultdoDiv = 239.42m / 0.3m;
+            decimal resultadoResiduo = 2321.432m % 23m;
+            double resultadoPoetencia = Math.Pow(78, 2);
+            double resultadoRaiz = Math.Sqrt(144);
+            Console.WriteLine("Valores {0}--{1}-{2}-{3}-{4}-{5}-{6}", resultadoSum,resultadoResta, resultadoMult, resultdoDiv, resultadoResiduo, resultadoPoetencia, resultadoRaiz);
+            int primerNumero = 48;
+            int segundoNumero = 12;
+            primerNumero += segundoNumero;
+            primerNumero -= segundoNumero;
+            primerNumero *= segundoNumero;
+            primerNumero /= segundoNumero;
+            primerNumero %= segundoNumero;
+            primerNumero++;
+            primerNumero--;
+            Console.WriteLine("Aqui puede ir cualquiera de los de arriba:" + primerNumero);
         }
     }
 }
